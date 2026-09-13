7c411533da0bfe74bff3a3b46e85aa7f1616c1ce
Md-Mahir-Asef <mdmahirasef.dev@gmail.com>
2026-09-13 09:48:51 +0600

    Can Read file and generate response now.

--- cleaners/quiz.cleaner.js
+++ cleaners/quiz.cleaner.js
@@ -1,24 +1,47 @@
 import { GoogleGenAI } from "@google/genai";
-import "dotenv/config";
-import { readdir } from "node:fs";
+import dotenv from "dotenv";
+import { readdir } from "node:fs/promises";
+import { join } from "node:path";
 
-async function imageProcessor() {
-  const images = await readdir("../quiz_images");
-  console.log(images);
-}
+dotenv.config({ path: join(import.meta.dirname, "..", ".env") });
 
 async function main() {
   const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
   const ai = new GoogleGenAI({
       apiKey: GEMINI_API_KEY,
   });
+  
+  const images = await readdir("../quiz_images");
+  console.log(images);
+  console.log(`Image File: `, images[0])
+  console.log("\n\n")
+
+  const uploadedFile = await ai.files.upload({
+    file: `../quiz_images/${images[0]}`,
+    config: { mimeType: "image/jpeg" },
+  });
+
+  let state = (await ai.files.get({ name: uploadedFile.name })).state;
+  while (state !== "ACTIVE") {
+    state = (await ai.files.get({ name: uploadedFile.name })).state;
+    if (state === "FAILED") throw new Error("File upload failed");
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  }
+
   const response = await ai.models.generateContent({
     model: 'gemini-3.6-flash',
-    contents: 'Can you help Extract me some questions in JSON format from an Image?',
+    contents: [
+      {
+        role: "user",
+        parts: [
+          { fileData: { fileUri: uploadedFile.uri, mimeType: uploadedFile.mimeType } },
+          { text: 'Can you view the contents of the uploaded image?' },
+        ],
+      },
+    ],
   });
 
   console.log(response.text);
 }
 
-imageProcessor()
-// main();
+main();
